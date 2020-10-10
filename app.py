69b144a6e922d4efbb4fb5b0b0040483d12b3019
chahatagrawal117 <chahatagrawal117@gmail.com>
2020-10-10 11:49:38 +0530

    checking only git-commands

--- app.py
+++ app.py
@@ -4,8 +4,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "<h1>This is the home page<h1>"
+    return "<h1>This is the home page created by chahat<h1>"
 
 
 if __name__ == '__main__':
     app.run(debug=True)
+
