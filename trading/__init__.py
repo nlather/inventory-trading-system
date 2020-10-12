1fd5c798f1aac376bbbddd13e63286d633d1c460
chahatagrawal117 <chahatagrawal117@gmail.com>
2020-10-13 02:24:30 +0530

    adding home,login,forgot_pasword,account pages and database

--- trading/__init__.py
+++ trading/__init__.py
@@ -19,8 +19,8 @@ login_manager.login_message_category = 'info'
 app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'laviagrawal117@gmail.com'
-app.config['MAIL_PASSWORD'] = 'chahat@1971'
+app.config['MAIL_USERNAME'] = '**@gmail.com'
+app.config['MAIL_PASSWORD'] = '**'
 mail = Mail(app)
 
 
