27fcadd9c488392db96e92767a9d9571c9284fa7
Nishant Raj Lather <lit2018038@iiitl.ac.in>
2020-12-02 02:14:39 +0530

    use env. variable for mail creds

--- trading/__init__.py
+++ trading/__init__.py
@@ -19,8 +19,8 @@ login_manager.login_message_category = 'info'
 app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = '**@gmail.com'
-app.config['MAIL_PASSWORD'] = '**'
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
 mail = Mail(app)
 
 from trading import routes
